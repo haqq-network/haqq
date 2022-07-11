11c20aa095e888ff0f62ba5db01ea5404b0ba61e
Evgeniy Abramov <i@kio.ninja>
2022-07-11 19:28:45 +0300

    feat: enable PoS

--- app/haqq_ante.go
+++ app/haqq_ante.go
@@ -5,14 +5,12 @@ import (
 	"strings"
 
 	"github.com/cosmos/cosmos-sdk/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-	authsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
-	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 	"github.com/cosmos/cosmos-sdk/x/staking/keeper"
-	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 )
 
-var ErrDelegationComingLater = sdkerrors.Register("haqq-ante", 6000, "delegation coming later")
 var ErrCommunitySpendingComingLater = sdkerrors.Register("haqq-ante", 6001, "community fund spend coming later")
 
 func NewHaqqAnteHandlerDecorator(sk keeper.Keeper, h types.AnteHandler) types.AnteHandler {
@@ -23,34 +21,6 @@ func NewHaqqAnteHandlerDecorator(sk keeper.Keeper, h types.AnteHandler) types.An
 			isValid := true
 
 			switch msgs[i].(type) {
-			case *stakingtypes.MsgDelegate, *stakingtypes.MsgCreateValidator:
-				isValid = false
-
-				if ctx.BlockHeight() == 0 {
-					continue
-				}
-
-				sigTx, ok := tx.(authsigning.SigVerifiableTx)
-				if !ok {
-					return ctx, sdkerrors.Wrap(sdkerrors.ErrTxDecode, "invalid tx type")
-				}
-
-				signers := sigTx.GetSigners()
-				validators := sk.GetAllValidators(ctx)
-
-				for j := 0; j < len(signers); j++ {
-					for k := 0; k < len(validators); k++ {
-						if signers[j].Equals(validators[k].GetOperator()) {
-							isValid = true
-							break
-						}
-					}
-				}
-
-				if !isValid {
-					return ctx, ErrDelegationComingLater
-				}
-
 			case *govtypes.MsgSubmitProposal:
 				disabledProposals := []string{"CommunityPoolSpendProposal"}
 				govMsg := msgs[i].(*govtypes.MsgSubmitProposal)
