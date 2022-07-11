c415d2e489998df9e7234a81dbfab4ef380e432b
Evgeniy Abramov <i@kio.ninja>
2022-07-11 18:59:25 +0300

    chore: better calc mint amount for fix total supply

--- app/test_helpers.go
+++ app/test_helpers.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"time"
 
+	"github.com/haqq-network/haqq/types"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	ibctesting "github.com/cosmos/ibc-go/v3/testing"
 	"github.com/cosmos/ibc-go/v3/testing/simapp"
@@ -80,7 +82,7 @@ func Setup(
 		// Initialize the chain
 		app.InitChain(
 			abci.RequestInitChain{
-				ChainId:         "evmos_9000-1",
+				ChainId:         types.MainNetChainID,
 				Validators:      []abci.ValidatorUpdate{},
 				ConsensusParams: DefaultConsensusParams,
 				AppStateBytes:   stateBytes,
