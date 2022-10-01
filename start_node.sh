232a95f6bd3d4d6160ad7f24d6ec2ac8e1dcc75a
cyberomanov <41644451+cyberomanov@users.noreply.github.com>
2022-10-01 10:01:28 +0400

    edit formating.

--- start_node.sh
+++ start_node.sh
@@ -12,9 +12,12 @@ then
   echo "Node config directory doesn't exist!"
 else
 
-haqqd start --pruning=nothing $TRACE --log_level $LOGLEVEL \
+haqqd start \
+--pruning=nothing $TRACE \
+--log_level $LOGLEVEL \
 --minimum-gas-prices=0.0001aISLM \
 --json-rpc.api eth,txpool,personal,net,debug,web3 \
---json-rpc.enable true --keyring-backend $KEYRING
+--json-rpc.enable true \
+--keyring-backend $KEYRING
 
 fi
