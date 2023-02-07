abe097079e74b60de280619ee54cff618b0891ab
Yuri Surbashev <yurist@insport.net>
2023-02-07 17:08:02 +0800

    fix: app export and PoA tests

--- app/app_test.go
+++ app/app_test.go
@@ -1,66 +1,106 @@
 package app
 
-// import (
-// 	"encoding/json"
-// 	"os"
-// 	"testing"
+import (
+	"encoding/json"
+	evm "github.com/evmos/ethermint/x/evm/types"
+	"os"
+	"testing"
 
-// 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
+	"github.com/cosmos/cosmos-sdk/simapp"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	"github.com/cosmos/ibc-go/v5/testing/mock"
+	"github.com/stretchr/testify/require"
+	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/tendermint/tendermint/libs/log"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	tmtypes "github.com/tendermint/tendermint/types"
+	dbm "github.com/tendermint/tm-db"
 
-// 	"github.com/stretchr/testify/require"
+	"github.com/evmos/ethermint/encoding"
+)
 
-// 	"github.com/cosmos/cosmos-sdk/simapp"
+func TestExport(t *testing.T) {
+	// create public key
+	privVal := mock.NewPV()
+	pubKey, err := privVal.GetPubKey()
+	require.NoError(t, err, "public key should be created without error")
 
-// 	abci "github.com/tendermint/tendermint/abci/types"
-// 	"github.com/tendermint/tendermint/libs/log"
-// 	dbm "github.com/tendermint/tm-db"
+	// create validator set with single validator
+	validator := tmtypes.NewValidator(pubKey, 1)
+	valSet := tmtypes.NewValidatorSet([]*tmtypes.Validator{validator})
 
-// 	"github.com/evmos/ethermint/encoding"
-// )
+	// generate genesis account
+	senderPrivKey := secp256k1.GenPrivKey()
+	acc := authtypes.NewBaseAccount(senderPrivKey.PubKey().Address().Bytes(), senderPrivKey.PubKey(), 0, 0)
+	balance := banktypes.Balance{
+		Address: acc.GetAddress().String(),
+		Coins:   sdk.NewCoins(sdk.NewCoin(evm.DefaultEVMDenom, sdk.NewInt(100000000000000))),
+	}
 
-// func TestExport(t *testing.T) {
-// 	db := dbm.NewMemDB()
-// 	app := NewHaqq(log.NewTMLogger(log.NewSyncWriter(os.Stdout)), db, nil, true, map[int64]bool{}, DefaultNodeHome, 0, encoding.MakeConfig(ModuleBasics), simapp.EmptyAppOptions{})
+	db := dbm.NewMemDB()
+	app := NewHaqq(log.NewTMLogger(log.NewSyncWriter(os.Stdout)), db, nil, true, map[int64]bool{}, DefaultNodeHome, 0, encoding.MakeConfig(ModuleBasics), simapp.EmptyAppOptions{})
 
-// 	genesisState := NewDefaultGenesisState()
-// 	stateBytes, err := json.MarshalIndent(genesisState, "", "  ")
-// 	require.NoError(t, err)
+	genesisState := NewDefaultGenesisState()
+	genesisState = GenesisStateWithValSet(app, genesisState, valSet, []authtypes.GenesisAccount{acc}, balance)
+	stateBytes, err := json.MarshalIndent(genesisState, "", "  ")
+	require.NoError(t, err)
 
-// 	// Initialize the chain
-// 	app.InitChain(
-// 		abci.RequestInitChain{
-// 			ChainId:       MainnetChainID + "-1",
-// 			Validators:    []abci.ValidatorUpdate{},
-// 			AppStateBytes: stateBytes,
-// 		},
-// 	)
-// 	app.Commit()
+	// Initialize the chain
+	app.InitChain(
+		abci.RequestInitChain{
+			ChainId:       MainnetChainID + "-1",
+			Validators:    []abci.ValidatorUpdate{},
+			AppStateBytes: stateBytes,
+		},
+	)
+	app.Commit()
 
-// 	// Making a new app object with the db, so that initchain hasn't been called
-// 	app2 := NewHaqq(log.NewTMLogger(log.NewSyncWriter(os.Stdout)), db, nil, true, map[int64]bool{}, DefaultNodeHome, 0, encoding.MakeConfig(ModuleBasics), simapp.EmptyAppOptions{})
-// 	_, err = app2.ExportAppStateAndValidators(false, []string{})
-// 	require.NoError(t, err, "ExportAppStateAndValidators should not have an error")
-// }
+	// Making a new app object with the db, so that initchain hasn't been called
+	app2 := NewHaqq(log.NewTMLogger(log.NewSyncWriter(os.Stdout)), db, nil, true, map[int64]bool{}, DefaultNodeHome, 0, encoding.MakeConfig(ModuleBasics), simapp.EmptyAppOptions{})
+	_, err = app2.ExportAppStateAndValidators(false, []string{})
+	require.NoError(t, err, "ExportAppStateAndValidators should not have an error")
+}
 
-// func TestPoA(t *testing.T) {
-// 	db := dbm.NewMemDB()
-// 	app := NewHaqq(log.NewTMLogger(log.NewSyncWriter(os.Stdout)), db, nil, true, map[int64]bool{}, DefaultNodeHome, 0, encoding.MakeConfig(ModuleBasics), simapp.EmptyAppOptions{})
+func TestPoA(t *testing.T) {
+	// create public key
+	privVal := mock.NewPV()
+	pubKey, err := privVal.GetPubKey()
+	require.NoError(t, err, "public key should be created without error")
 
-// 	genesisState := NewDefaultGenesisState()
-// 	stateBytes, err := json.MarshalIndent(genesisState, "", "  ")
-// 	require.NoError(t, err)
+	// create validator set with single validator
+	validator := tmtypes.NewValidator(pubKey, 1)
+	valSet := tmtypes.NewValidatorSet([]*tmtypes.Validator{validator})
 
-// 	// Initialize the chain
-// 	app.InitChain(
-// 		abci.RequestInitChain{
-// 			ChainId:       MainnetChainID + "-1",
-// 			Validators:    []abci.ValidatorUpdate{},
-// 			AppStateBytes: stateBytes,
-// 		},
-// 	)
-// 	app.Commit()
+	// generate genesis account
+	senderPrivKey := secp256k1.GenPrivKey()
+	acc := authtypes.NewBaseAccount(senderPrivKey.PubKey().Address().Bytes(), senderPrivKey.PubKey(), 0, 0)
+	balance := banktypes.Balance{
+		Address: acc.GetAddress().String(),
+		Coins:   sdk.NewCoins(sdk.NewCoin(evm.DefaultEVMDenom, sdk.NewInt(100000000000000))),
+	}
 
-// 	ctx := app.NewUncachedContext(false, tmproto.Header{})
-// 	validatorUpdates := app.StakingKeeper.BlockValidatorUpdates(ctx)
-// 	require.Equal(t, len(validatorUpdates), 0)
-// }
+	db := dbm.NewMemDB()
+	app := NewHaqq(log.NewTMLogger(log.NewSyncWriter(os.Stdout)), db, nil, true, map[int64]bool{}, DefaultNodeHome, 0, encoding.MakeConfig(ModuleBasics), simapp.EmptyAppOptions{})
+
+	genesisState := NewDefaultGenesisState()
+	genesisState = GenesisStateWithValSet(app, genesisState, valSet, []authtypes.GenesisAccount{acc}, balance)
+	stateBytes, err := json.MarshalIndent(genesisState, "", "  ")
+	require.NoError(t, err)
+
+	// Initialize the chain
+	app.InitChain(
+		abci.RequestInitChain{
+			ChainId:       MainnetChainID + "-1",
+			Validators:    []abci.ValidatorUpdate{},
+			AppStateBytes: stateBytes,
+		},
+	)
+	app.Commit()
+
+	ctx := app.NewUncachedContext(false, tmproto.Header{})
+	validatorUpdates := app.StakingKeeper.BlockValidatorUpdates(ctx)
+	require.Equal(t, len(validatorUpdates), 0)
+}
