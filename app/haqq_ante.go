e334c65be133b4b25d75f6462037e24dba146ed0
Yuri Surbashev <yurist@insport.net>
2023-05-31 16:58:57 +0800

    chore: enable proof-of-stake for all networks

--- app/haqq_ante.go
+++ app/haqq_ante.go
@@ -8,14 +8,10 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/staking/keeper"
 
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-	authsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
-	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
-	haqqtypes "github.com/haqq-network/haqq/types"
 )
 
 var (
-	ErrDelegationComingLater        = sdkerrors.Register("haqq-ante", 6000, "delegation coming later")
 	ErrCommunitySpendingComingLater = sdkerrors.Register("haqq-ante", 6001, "community fund spend coming later")
 )
 
@@ -27,36 +23,6 @@ func NewHaqqAnteHandlerDecorator(sk keeper.Keeper, h types.AnteHandler) types.An
 			isValid := true
 
 			switch msgs[i].(type) {
-			case *stakingtypes.MsgDelegate, *stakingtypes.MsgCreateValidator:
-				if haqqtypes.IsMainNetwork(ctx.ChainID()) || haqqtypes.IsTestEdge1Network(ctx.ChainID()) {
-					isValid = false
-
-					if ctx.BlockHeight() == 0 {
-						continue
-					}
-
-					sigTx, ok := tx.(authsigning.SigVerifiableTx)
-					if !ok {
-						return ctx, sdkerrors.Wrap(sdkerrors.ErrTxDecode, "invalid tx type")
-					}
-
-					signers := sigTx.GetSigners()
-					validators := sk.GetAllValidators(ctx)
-
-					for j := 0; j < len(signers); j++ {
-						for k := 0; k < len(validators); k++ {
-							if signers[j].Equals(validators[k].GetOperator()) {
-								isValid = true
-								break
-							}
-						}
-					}
-
-					if !isValid {
-						return ctx, ErrDelegationComingLater
-					}
-				}
-
 			case *govtypes.MsgSubmitProposal:
 				disabledProposals := []string{"CommunityPoolSpendProposal"}
 				govMsg := msgs[i].(*govtypes.MsgSubmitProposal)
