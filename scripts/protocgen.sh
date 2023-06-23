d01fdf6b03be5519b4552059138eb4a521c10b43
Yuri Surbashev <yurist@insport.net>
2023-06-23 18:48:12 +0800

    chore: clean commented code

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -2,37 +2,6 @@
 
 set -eo pipefail
 
-# protoc_gen_gocosmos() {
-#   if ! grep "github.com/gogo/protobuf => github.com/regen-network/protobuf" go.mod &>/dev/null ; then
-#     echo -e "\tPlease run this command from somewhere inside the sommelier folder."
-#     return 1
-#   fi
-
-#   go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
-# }
-
-# protoc_gen_doc() {
-#   go get -u github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc 2>/dev/null
-# }
-
-# protoc_gen_gocosmos
-# protoc_gen_doc
-
-# proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
-# # TODO: migrate to `buf build`
-# for dir in $proto_dirs; do
-#   buf protoc \
-#   -I "proto" \
-#   -I "third_party/proto" \
-#   --gocosmos_out=plugins=interfacetype+grpc,\
-# Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
-#   --grpc-gateway_out=logtostderr=true:. \
-#   $(find "${dir}" -maxdepth 1 -name '*.proto')
-
-# done
-
-#!/usr/bin/env bash
-
 # --------------
 # Commands to run locally
 # docker run --network host --rm -v $(CURDIR):/workspace --workdir /workspace tendermintdev/sdk-proto-gen:v0.7 sh ./protocgen.sh
@@ -50,15 +19,6 @@ for dir in $proto_dirs; do
     fi
   done
 done
-
-# command to generate docs using protoc-gen-doc
-# TODO: migrate to `buf build`
-# buf protoc \
-# -I "proto" \
-# -I "third_party/proto" \
-# # --doc_out=./docs/protocol \
-# # --doc_opt=./docs/protodoc-markdown.tmpl,proto-docs.md \
-# $(find "$(pwd)/proto" -maxdepth 5 -name '*.proto')
 
 # move proto files to the right places
 cp -r github.com/haqq-network/haqq/* ./
