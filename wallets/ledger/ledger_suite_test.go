b7b4fc91c506fe353c460d694bdb44dbd0e75dd9
Yuri Surbashev <yurist@insport.net>
2023-10-04 03:46:50 +0800

    chore: pull ledger

---
+++ wallets/ledger/ledger_suite_test.go
@@ -0,0 +1,152 @@
+package ledger_test
+
+import (
+	"encoding/hex"
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	codecTypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
+	cryptoTypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	txTypes "github.com/cosmos/cosmos-sdk/types/tx"
+	"github.com/cosmos/cosmos-sdk/types/tx/signing"
+	auxTx "github.com/cosmos/cosmos-sdk/x/auth/tx"
+	bankTypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+
+	"github.com/haqq-network/haqq/utils"
+	"github.com/haqq-network/haqq/wallets/ledger"
+	"github.com/haqq-network/haqq/wallets/ledger/mocks"
+	"github.com/haqq-network/haqq/wallets/usbwallet"
+)
+
+type LedgerTestSuite struct {
+	suite.Suite
+	txAmino    []byte
+	txProtobuf []byte
+	ledger     ledger.HaqqSECP256K1
+	mockWallet *mocks.Wallet
+	hrp        string
+}
+
+func TestLedgerTestSuite(t *testing.T) {
+	suite.Run(t, new(LedgerTestSuite))
+}
+
+func (suite *LedgerTestSuite) SetupTest() {
+	suite.hrp = "haqq"
+
+	suite.txAmino = suite.getMockTxAmino()
+	suite.txProtobuf = suite.getMockTxProtobuf()
+
+	hub, err := usbwallet.NewLedgerHub()
+	suite.Require().NoError(err)
+
+	mockWallet := new(mocks.Wallet)
+	suite.mockWallet = mockWallet
+	suite.ledger = ledger.HaqqSECP256K1{Hub: hub, PrimaryWallet: mockWallet}
+}
+
+func (suite *LedgerTestSuite) newPubKey(pk string) (res cryptoTypes.PubKey) {
+	pkBytes, err := hex.DecodeString(pk)
+	suite.Require().NoError(err)
+
+	pubkey := &ed25519.PubKey{Key: pkBytes}
+
+	return pubkey
+}
+
+func (suite *LedgerTestSuite) getMockTxAmino() []byte {
+	whitespaceRegex := regexp.MustCompile(`\s+`)
+	tmp := whitespaceRegex.ReplaceAllString(
+		`{
+			"account_number": "0",
+			"chain_id":"haqq_54211-1",
+			"fee":{
+				"amount":[{"amount":"150","denom":"aISLM"}],
+				"gas":"20000"
+			},
+			"memo":"memo",
+			"msgs":[{
+				"type":"cosmos-sdk/MsgSend",
+				"value":{
+					"amount":[{"amount":"150","denom":"aISLM"}],
+					"from_address":"haqq1tjdjfavsy956d25hvhs3p0nw9a7pfghqm0up92",
+					"to_address":"haqq1hdr0lhv75vesvtndlh78ck4cez6esz8u2lk0hq"
+				}
+			}],
+			"sequence":"6"
+		}`,
+		"",
+	)
+
+	return []byte(tmp)
+}
+
+func (suite *LedgerTestSuite) getMockTxProtobuf() []byte {
+	marshaler := codec.NewProtoCodec(codecTypes.NewInterfaceRegistry())
+
+	memo := "memo"
+	msg := bankTypes.NewMsgSend(
+		sdk.MustAccAddressFromBech32("haqq1tjdjfavsy956d25hvhs3p0nw9a7pfghqm0up92"),
+		sdk.MustAccAddressFromBech32("haqq1hdr0lhv75vesvtndlh78ck4cez6esz8u2lk0hq"),
+		[]sdk.Coin{
+			{
+				Denom:  utils.BaseDenom,
+				Amount: sdk.NewIntFromUint64(150),
+			},
+		},
+	)
+
+	msgAsAny, err := codecTypes.NewAnyWithValue(msg)
+	suite.Require().NoError(err)
+
+	body := &txTypes.TxBody{
+		Messages: []*codecTypes.Any{
+			msgAsAny,
+		},
+		Memo: memo,
+	}
+
+	pubKey := suite.newPubKey("0B485CFC0EECC619440448436F8FC9DF40566F2369E72400281454CB552AFB50")
+
+	pubKeyAsAny, err := codecTypes.NewAnyWithValue(pubKey)
+	suite.Require().NoError(err)
+
+	signingMode := txTypes.ModeInfo_Single_{
+		Single: &txTypes.ModeInfo_Single{
+			Mode: signing.SignMode_SIGN_MODE_DIRECT,
+		},
+	}
+
+	signerInfo := &txTypes.SignerInfo{
+		PublicKey: pubKeyAsAny,
+		ModeInfo: &txTypes.ModeInfo{
+			Sum: &signingMode,
+		},
+		Sequence: 6,
+	}
+
+	fee := txTypes.Fee{Amount: sdk.NewCoins(sdk.NewInt64Coin(utils.BaseDenom, 150)), GasLimit: 20000}
+
+	authInfo := &txTypes.AuthInfo{
+		SignerInfos: []*txTypes.SignerInfo{signerInfo},
+		Fee:         &fee,
+	}
+
+	bodyBytes := marshaler.MustMarshal(body)
+	authInfoBytes := marshaler.MustMarshal(authInfo)
+
+	signBytes, err := auxTx.DirectSignBytes(
+		bodyBytes,
+		authInfoBytes,
+		"haqq_54211-1",
+		0,
+	)
+	suite.Require().NoError(err)
+
+	return signBytes
+}
