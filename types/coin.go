8a6a15e71b81cc8db9633bae6884817f6fa46636
Yuri Surbashev <yurist@insport.net>
2023-10-04 03:10:17 +0800

    chore: pull core types

---
+++ types/coin.go
@@ -0,0 +1,48 @@
+package types
+
+import (
+	"math/big"
+
+	sdkmath "cosmossdk.io/math"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+const (
+	// AttoIslm defines the default coin denomination used in Haqq in:
+	//
+	// - Staking parameters: denomination used as stake in the dPoS chain
+	// - Mint parameters: denomination minted due to fee distribution rewards
+	// - Governance parameters: denomination used for spam prevention in proposal deposits
+	// - Crisis parameters: constant fee denomination used for spam prevention to check broken invariant
+	// - EVM parameters: denomination used for running EVM state transitions in Haqq.
+	AttoIslm string = "aISLM"
+
+	// BaseDenomUnit defines the base denomination unit for Haqq.
+	// 1 ISLM = 1x10^{BaseDenomUnit} aISLM
+	BaseDenomUnit = 18
+
+	// DefaultGasPrice is default gas price for evm transactions
+	DefaultGasPrice = 20
+)
+
+// PowerReduction defines the default power reduction value for staking
+var PowerReduction = sdkmath.NewIntFromBigInt(new(big.Int).Exp(big.NewInt(10), big.NewInt(BaseDenomUnit), nil))
+
+// NewIslmCoin is a utility function that returns an "aISLM" coin with the given sdkmath.Int amount.
+// The function will panic if the provided amount is negative.
+func NewIslmCoin(amount sdkmath.Int) sdk.Coin {
+	return sdk.NewCoin(AttoIslm, amount)
+}
+
+// NewIslmDecCoin is a utility function that returns an "aISLM" decimal coin with the given sdkmath.Int amount.
+// The function will panic if the provided amount is negative.
+func NewIslmDecCoin(amount sdkmath.Int) sdk.DecCoin {
+	return sdk.NewDecCoin(AttoIslm, amount)
+}
+
+// NewIslmCoinInt64 is a utility function that returns an "aISLM" coin with the given int64 amount.
+// The function will panic if the provided amount is negative.
+func NewIslmCoinInt64(amount int64) sdk.Coin {
+	return sdk.NewInt64Coin(AttoIslm, amount)
+}
---
+++ types/indexer.go
@@ -0,0 +1,19 @@
+package types
+
+import (
+	abci "github.com/cometbft/cometbft/abci/types"
+	tmtypes "github.com/cometbft/cometbft/types"
+	"github.com/ethereum/go-ethereum/common"
+)
+
+// EVMTxIndexer defines the interface of custom eth tx indexer.
+type EVMTxIndexer interface {
+	// LastIndexedBlock returns -1 if indexer db is empty
+	LastIndexedBlock() (int64, error)
+	IndexBlock(*tmtypes.Block, []*abci.ResponseDeliverTx) error
+
+	// GetByTxHash returns nil if tx not found.
+	GetByTxHash(common.Hash) (*TxResult, error)
+	// GetByBlockAndIndex returns nil if tx not found.
+	GetByBlockAndIndex(int64, int32) (*TxResult, error)
+}
