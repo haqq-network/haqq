2f878df8a15d053f571d3d544f5f6e43cbd703e1
Yuri Surbashev <yurist@insport.net>
2023-10-05 01:35:14 +0800

    chore: pull client

--- client/docs/statik/init.go
+++ client/docs/statik/init.go
@@ -1,3 +1,3 @@
 package statik
 
-// This just for fixing the error in importing empty github.com/evmos/ethermint/client/docs/statik
+// This just for fixing the error in importing empty github.com/haqq-network/haqq/client/docs/statik
---
+++ client/keys/utils.go
@@ -0,0 +1,58 @@
+package keys
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+
+	"sigs.k8s.io/yaml"
+
+	cryptokeyring "github.com/cosmos/cosmos-sdk/crypto/keyring"
+)
+
+// available output formats.
+const (
+	OutputFormatText = "text"
+	OutputFormatJSON = "json"
+)
+
+type bechKeyOutFn func(k *cryptokeyring.Record) (cryptokeyring.KeyOutput, error)
+
+func printKeyringRecord(w io.Writer, k *cryptokeyring.Record, bechKeyOut bechKeyOutFn, output string) error {
+	ko, err := bechKeyOut(k)
+	if err != nil {
+		return err
+	}
+
+	switch output {
+	case OutputFormatText:
+		if err := printTextRecords(w, []cryptokeyring.KeyOutput{ko}); err != nil {
+			return err
+		}
+
+	case OutputFormatJSON:
+		out, err := json.Marshal(ko)
+		if err != nil {
+			return err
+		}
+
+		if _, err := fmt.Fprintln(w, string(out)); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func printTextRecords(w io.Writer, kos []cryptokeyring.KeyOutput) error {
+	out, err := yaml.Marshal(&kos)
+	if err != nil {
+		return err
+	}
+
+	if _, err := fmt.Fprintln(w, string(out)); err != nil {
+		return err
+	}
+
+	return nil
+}
