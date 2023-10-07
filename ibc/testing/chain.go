41a08112eb038ff6c8718aad2afd1dd4603427dc
Yuri Surbashev <yurist@insport.net>
2023-10-07 16:46:05 +0800

    chore: pull ibc

---
+++ ibc/testing/chain.go
@@ -0,0 +1,115 @@
+package ibctesting
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	channeltypes "github.com/cosmos/ibc-go/v6/modules/core/04-channel/types"
+	ibcgotesting "github.com/cosmos/ibc-go/v6/testing"
+	"github.com/cosmos/ibc-go/v6/testing/mock"
+	"github.com/ethereum/go-ethereum/common"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	tmtypes "github.com/tendermint/tendermint/types"
+
+	"github.com/haqq-network/haqq/crypto/ethsecp256k1"
+	haqqtypes "github.com/haqq-network/haqq/types"
+	"github.com/haqq-network/haqq/utils"
+	evmtypes "github.com/haqq-network/haqq/x/evm/types"
+)
+
+// ChainIDPrefix defines the default chain ID prefix for Evmos test chains
+var ChainIDPrefix = "haqq_11235-"
+
+func init() {
+	ibcgotesting.ChainIDPrefix = ChainIDPrefix
+}
+
+// NewTestChain initializes a new TestChain instance with a single validator set using a
+// generated private key. It also creates a sender account to be used for delivering transactions.
+//
+// The first block height is committed to state in order to allow for client creations on
+// counterparty chains. The TestChain will return with a block height starting at 2.
+//
+// Time management is handled by the Coordinator in order to ensure synchrony between chains.
+// Each update of any chain increments the block header time for all chains by 5 seconds.
+func NewTestChain(t *testing.T, coord *ibcgotesting.Coordinator, chainID string) *ibcgotesting.TestChain {
+	// generate validator private/public key
+	privVal := mock.NewPV()
+	pubKey, err := privVal.GetPubKey()
+	require.NoError(t, err)
+
+	// create validator set with single validator
+	validator := tmtypes.NewValidator(pubKey, 1)
+	valSet := tmtypes.NewValidatorSet([]*tmtypes.Validator{validator})
+	signers := make(map[string]tmtypes.PrivValidator)
+	signers[pubKey.Address().String()] = privVal
+
+	// generate genesis account
+	senderPrivKey, err := ethsecp256k1.GenerateKey()
+	if err != nil {
+		panic(err)
+	}
+
+	baseAcc := authtypes.NewBaseAccount(senderPrivKey.PubKey().Address().Bytes(), senderPrivKey.PubKey(), 0, 0)
+
+	acc := &haqqtypes.EthAccount{
+		BaseAccount: baseAcc,
+		CodeHash:    common.BytesToHash(evmtypes.EmptyCodeHash).Hex(),
+	}
+
+	amount := sdk.TokensFromConsensusPower(1, haqqtypes.PowerReduction)
+
+	balance := banktypes.Balance{
+		Address: acc.GetAddress().String(),
+		Coins:   sdk.NewCoins(sdk.NewCoin(utils.BaseDenom, amount)),
+	}
+
+	app := SetupWithGenesisValSet(t, valSet, []authtypes.GenesisAccount{acc}, chainID, balance)
+
+	// create current header and call begin block
+	header := tmproto.Header{
+		ChainID: chainID,
+		Height:  1,
+		Time:    coord.CurrentTime.UTC(),
+	}
+
+	txConfig := app.GetTxConfig()
+
+	// create an account to send transactions from
+	chain := &ibcgotesting.TestChain{
+		T:             t,
+		Coordinator:   coord,
+		ChainID:       chainID,
+		App:           app,
+		CurrentHeader: header,
+		QueryServer:   app.GetIBCKeeper(),
+		TxConfig:      txConfig,
+		Codec:         app.AppCodec(),
+		Vals:          valSet,
+		Signers:       signers,
+		SenderPrivKey: senderPrivKey,
+		SenderAccount: acc,
+		NextVals:      valSet,
+	}
+
+	coord.CommitBlock(chain)
+
+	return chain
+}
+
+func NewTransferPath(chainA, chainB *ibcgotesting.TestChain) *Path {
+	path := NewPath(chainA, chainB)
+	path.EndpointA.ChannelConfig.PortID = ibcgotesting.TransferPort
+	path.EndpointB.ChannelConfig.PortID = ibcgotesting.TransferPort
+
+	path.EndpointA.ChannelConfig.Order = channeltypes.UNORDERED
+	path.EndpointB.ChannelConfig.Order = channeltypes.UNORDERED
+	path.EndpointA.ChannelConfig.Version = "ics20-1"
+	path.EndpointB.ChannelConfig.Version = "ics20-1"
+
+	return path
+}
---
+++ ibc/utils_test.go
@@ -0,0 +1,318 @@
+package ibc
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	transfertypes "github.com/cosmos/ibc-go/v6/modules/apps/transfer/types"
+	channeltypes "github.com/cosmos/ibc-go/v6/modules/core/04-channel/types"
+	ibctesting "github.com/cosmos/ibc-go/v6/testing"
+
+	teststypes "github.com/haqq-network/haqq/types/tests"
+)
+
+func init() {
+	cfg := sdk.GetConfig()
+	cfg.SetBech32PrefixForAccount("haqq", "haqqpub")
+}
+
+func TestGetTransferSenderRecipient(t *testing.T) {
+	testCases := []struct {
+		name         string
+		packet       channeltypes.Packet
+		expSender    string
+		expRecipient string
+		expError     bool
+	}{
+		{
+			"empty packet",
+			channeltypes.Packet{},
+			"", "",
+			true,
+		},
+		{
+			"invalid packet data",
+			channeltypes.Packet{
+				Data: ibctesting.MockFailPacketData,
+			},
+			"", "",
+			true,
+		},
+		{
+			"empty FungibleTokenPacketData",
+			channeltypes.Packet{
+				Data: transfertypes.ModuleCdc.MustMarshalJSON(
+					&transfertypes.FungibleTokenPacketData{},
+				),
+			},
+			"", "",
+			true,
+		},
+		{
+			"invalid sender",
+			channeltypes.Packet{
+				Data: transfertypes.ModuleCdc.MustMarshalJSON(
+					&transfertypes.FungibleTokenPacketData{
+						Sender:   "cosmos1",
+						Receiver: "haqq1hdr0lhv75vesvtndlh78ck4cez6esz8u2lk0hq",
+						Amount:   "123456",
+					},
+				),
+			},
+			"", "",
+			true,
+		},
+		{
+			"invalid recipient",
+			channeltypes.Packet{
+				Data: transfertypes.ModuleCdc.MustMarshalJSON(
+					&transfertypes.FungibleTokenPacketData{
+						Sender:   "cosmos1qql8ag4cluz6r4dz28p3w00dnc9w8ueulg2gmc",
+						Receiver: "haqq1",
+						Amount:   "123456",
+					},
+				),
+			},
+			"", "",
+			true,
+		},
+		{
+			"valid - cosmos sender, haqq recipient",
+			channeltypes.Packet{
+				Data: transfertypes.ModuleCdc.MustMarshalJSON(
+					&transfertypes.FungibleTokenPacketData{
+						Sender:   "cosmos1qql8ag4cluz6r4dz28p3w00dnc9w8ueulg2gmc",
+						Receiver: "haqq1hdr0lhv75vesvtndlh78ck4cez6esz8u2lk0hq",
+						Amount:   "123456",
+					},
+				),
+			},
+			"haqq1tjdjfavsy956d25hvhs3p0nw9a7pfghqm0up92",
+			"haqq1hdr0lhv75vesvtndlh78ck4cez6esz8u2lk0hq",
+			false,
+		},
+		{
+			"valid - haqq sender, cosmos recipient",
+			channeltypes.Packet{
+				Data: transfertypes.ModuleCdc.MustMarshalJSON(
+					&transfertypes.FungibleTokenPacketData{
+						Sender:   "haqq1hdr0lhv75vesvtndlh78ck4cez6esz8u2lk0hq",
+						Receiver: "cosmos1qql8ag4cluz6r4dz28p3w00dnc9w8ueulg2gmc",
+						Amount:   "123456",
+					},
+				),
+			},
+			"haqq1hdr0lhv75vesvtndlh78ck4cez6esz8u2lk0hq",
+			"haqq1tjdjfavsy956d25hvhs3p0nw9a7pfghqm0up92",
+			false,
+		},
+		{
+			"valid - osmosis sender, haqq recipient",
+			channeltypes.Packet{
+				Data: transfertypes.ModuleCdc.MustMarshalJSON(
+					&transfertypes.FungibleTokenPacketData{
+						Sender:   "osmo1qql8ag4cluz6r4dz28p3w00dnc9w8ueuhnecd2",
+						Receiver: "haqq1hdr0lhv75vesvtndlh78ck4cez6esz8u2lk0hq",
+						Amount:   "123456",
+					},
+				),
+			},
+			"haqq1tjdjfavsy956d25hvhs3p0nw9a7pfghqm0up92",
+			"haqq1hdr0lhv75vesvtndlh78ck4cez6esz8u2lk0hq",
+			false,
+		},
+	}
+
+	for _, tc := range testCases {
+		sender, recipient, _, _, err := GetTransferSenderRecipient(tc.packet)
+		if tc.expError {
+			require.Error(t, err, tc.name)
+		} else {
+			require.NoError(t, err, tc.name)
+			require.Equal(t, tc.expSender, sender.String())
+			require.Equal(t, tc.expRecipient, recipient.String())
+		}
+	}
+}
+
+func TestGetTransferAmount(t *testing.T) {
+	testCases := []struct {
+		name      string
+		packet    channeltypes.Packet
+		expAmount string
+		expError  bool
+	}{
+		{
+			"empty packet",
+			channeltypes.Packet{},
+			"",
+			true,
+		},
+		{
+			"invalid packet data",
+			channeltypes.Packet{
+				Data: ibctesting.MockFailPacketData,
+			},
+			"",
+			true,
+		},
+		{
+			"invalid amount - empty",
+			channeltypes.Packet{
+				Data: transfertypes.ModuleCdc.MustMarshalJSON(
+					&transfertypes.FungibleTokenPacketData{
+						Sender:   "cosmos1qql8ag4cluz6r4dz28p3w00dnc9w8ueulg2gmc",
+						Receiver: "haqq1hdr0lhv75vesvtndlh78ck4cez6esz8u2lk0hq",
+						Amount:   "",
+					},
+				),
+			},
+			"",
+			true,
+		},
+		{
+			"invalid amount - non-int",
+			channeltypes.Packet{
+				Data: transfertypes.ModuleCdc.MustMarshalJSON(
+					&transfertypes.FungibleTokenPacketData{
+						Sender:   "cosmos1qql8ag4cluz6r4dz28p3w00dnc9w8ueulg2gmc",
+						Receiver: "haqq1hdr0lhv75vesvtndlh78ck4cez6esz8u2lk0hq",
+						Amount:   "test",
+					},
+				),
+			},
+			"test",
+			true,
+		},
+		{
+			"valid",
+			channeltypes.Packet{
+				Data: transfertypes.ModuleCdc.MustMarshalJSON(
+					&transfertypes.FungibleTokenPacketData{
+						Sender:   "cosmos1qql8ag4cluz6r4dz28p3w00dnc9w8ueulg2gmc",
+						Receiver: "haqq1hdr0lhv75vesvtndlh78ck4cez6esz8u2lk0hq",
+						Amount:   "10000",
+					},
+				),
+			},
+			"10000",
+			false,
+		},
+	}
+
+	for _, tc := range testCases {
+		amt, err := GetTransferAmount(tc.packet)
+		if tc.expError {
+			require.Error(t, err, tc.name)
+		} else {
+			require.NoError(t, err, tc.name)
+			require.Equal(t, tc.expAmount, amt)
+		}
+	}
+}
+
+func TestGetReceivedCoin(t *testing.T) {
+	testCases := []struct {
+		name       string
+		srcPort    string
+		srcChannel string
+		dstPort    string
+		dstChannel string
+		rawDenom   string
+		rawAmount  string
+		expCoin    sdk.Coin
+	}{
+		{
+			"transfer unwrapped coin to destination which is not its source",
+			"transfer",
+			"channel-0",
+			"transfer",
+			"channel-0",
+			"uosmo",
+			"10",
+			sdk.Coin{Denom: teststypes.UosmoIbcdenom, Amount: sdk.NewInt(10)},
+		},
+		{
+			"transfer ibc wrapped coin to destination which is its source",
+			"transfer",
+			"channel-0",
+			"transfer",
+			"channel-0",
+			"transfer/channel-0/aISLM",
+			"10",
+			sdk.Coin{Denom: "aISLM", Amount: sdk.NewInt(10)},
+		},
+		{
+			"transfer 2x ibc wrapped coin to destination which is its source",
+			"transfer",
+			"channel-0",
+			"transfer",
+			"channel-2",
+			"transfer/channel-0/transfer/channel-1/uatom",
+			"10",
+			sdk.Coin{Denom: teststypes.UatomIbcdenom, Amount: sdk.NewInt(10)},
+		},
+		{
+			"transfer ibc wrapped coin to destination which is not its source",
+			"transfer",
+			"channel-0",
+			"transfer",
+			"channel-0",
+			"transfer/channel-1/uatom",
+			"10",
+			sdk.Coin{Denom: teststypes.UatomOsmoIbcdenom, Amount: sdk.NewInt(10)},
+		},
+	}
+
+	for _, tc := range testCases {
+		coin := GetReceivedCoin(tc.srcPort, tc.srcChannel, tc.dstPort, tc.dstChannel, tc.rawDenom, tc.rawAmount)
+		require.Equal(t, tc.expCoin, coin)
+	}
+}
+
+func TestGetSentCoin(t *testing.T) {
+	testCases := []struct {
+		name      string
+		rawDenom  string
+		rawAmount string
+		expCoin   sdk.Coin
+	}{
+		{
+			"get unwrapped aISLM coin",
+			"aISLM",
+			"10",
+			sdk.Coin{Denom: "aISLM", Amount: sdk.NewInt(10)},
+		},
+		{
+			"get ibc wrapped aISLM coin",
+			"transfer/channel-0/aISLM",
+			"10",
+			sdk.Coin{Denom: teststypes.AislmIbcdenom, Amount: sdk.NewInt(10)},
+		},
+		{
+			"get ibc wrapped uosmo coin",
+			"transfer/channel-0/uosmo",
+			"10",
+			sdk.Coin{Denom: teststypes.UosmoIbcdenom, Amount: sdk.NewInt(10)},
+		},
+		{
+			"get ibc wrapped uatom coin",
+			"transfer/channel-1/uatom",
+			"10",
+			sdk.Coin{Denom: teststypes.UatomIbcdenom, Amount: sdk.NewInt(10)},
+		},
+		{
+			"get 2x ibc wrapped uatom coin",
+			"transfer/channel-0/transfer/channel-1/uatom",
+			"10",
+			sdk.Coin{Denom: teststypes.UatomOsmoIbcdenom, Amount: sdk.NewInt(10)},
+		},
+	}
+
+	for _, tc := range testCases {
+		coin := GetSentCoin(tc.rawDenom, tc.rawAmount)
+		require.Equal(t, tc.expCoin, coin)
+	}
+}
