a8307ee645568abaf6b87de7f4f3bcdadcd27507
Yuri Surbashev <yurist@insport.net>
2023-10-07 23:00:59 +0800

    chore: pull evm

---
+++ x/evm/keeper/params.go
@@ -0,0 +1,41 @@
+package keeper
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/haqq-network/haqq/x/evm/types"
+)
+
+// GetParams returns the total set of evm parameters.
+func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
+	store := ctx.KVStore(k.storeKey)
+	bz := store.Get(types.KeyPrefixParams)
+	if len(bz) == 0 {
+		return k.GetLegacyParams(ctx)
+	}
+	k.cdc.MustUnmarshal(bz, &params)
+	return
+}
+
+// SetParams sets the EVM params each in their individual key for better get performance
+func (k Keeper) SetParams(ctx sdk.Context, params types.Params) error {
+	if err := params.Validate(); err != nil {
+		return err
+	}
+
+	store := ctx.KVStore(k.storeKey)
+	bz, err := k.cdc.Marshal(&params)
+	if err != nil {
+		return err
+	}
+
+	store.Set(types.KeyPrefixParams, bz)
+	return nil
+}
+
+// GetLegacyParams returns param set for version before migrate
+func (k Keeper) GetLegacyParams(ctx sdk.Context) types.Params {
+	var params types.Params
+	k.ss.GetParamSetIfExists(ctx, &params)
+	return params
+}
