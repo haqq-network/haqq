d516c14d9b8e294542709a7ac2fbb85c7513f4c1
Yuri Surbashev <yurist@insport.net>
2023-10-08 08:57:06 +0800

    chore: lift vesting restrictions

--- app/haqq_ante.go
+++ app/haqq_ante.go
@@ -6,17 +6,12 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-	authsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
 	"github.com/cosmos/cosmos-sdk/x/staking/keeper"
-
-	haqqtypes "github.com/haqq-network/haqq/utils"
-	vestingtypes "github.com/haqq-network/haqq/x/vesting/types"
 )
 
 var (
 	ErrCommunitySpendingComingLater = sdkerrors.Register("haqq-ante", 6001, "community fund spend coming later")
-	ErrVestingComingLater           = sdkerrors.Register("haqq-ante", 6002, "vesting coming later")
 )
 
 func NewHaqqAnteHandlerDecorator(_ keeper.Keeper, h types.AnteHandler) types.AnteHandler {
@@ -27,23 +22,6 @@ func NewHaqqAnteHandlerDecorator(_ keeper.Keeper, h types.AnteHandler) types.Ant
 			isValid := true
 
 			switch msg := msgs[i].(type) {
-			case *vestingtypes.MsgConvertIntoVestingAccount, *vestingtypes.MsgCreateClawbackVestingAccount:
-				sigTx, ok := tx.(authsigning.SigVerifiableTx)
-				if !ok {
-					return ctx, sdkerrors.Wrap(sdkerrors.ErrTxDecode, "invalid tx type")
-				}
-
-				signers := sigTx.GetSigners()
-				for j := 0; j < len(signers); j++ {
-					if !haqqtypes.IsAllowedVestingFunderAccount(signers[j].String()) {
-						isValid = false
-						break
-					}
-				}
-
-				if !isValid {
-					return ctx, ErrVestingComingLater
-				}
 			case *govtypes.MsgSubmitProposal:
 				disabledProposals := []string{"CommunityPoolSpendProposal"}
 
