b724227e2cd994ede428079893ff90f6cc1172d7
Yuri Surbashev <yurist@insport.net>
2023-10-08 08:16:27 +0800

    chore: add migrations

--- app/upgrades/v1.6.0/upgrades.go
+++ app/upgrades/v1.6.0/upgrades.go
@@ -5,6 +5,19 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/module"
 	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	genesistypes "github.com/cosmos/ibc-go/v6/modules/apps/27-interchain-accounts/genesis/types"
+	transfertypes "github.com/cosmos/ibc-go/v6/modules/apps/transfer/types"
+
+	ica "github.com/cosmos/ibc-go/v6/modules/apps/27-interchain-accounts"
+	icahosttypes "github.com/cosmos/ibc-go/v6/modules/apps/27-interchain-accounts/host/types"
+	icatypes "github.com/cosmos/ibc-go/v6/modules/apps/27-interchain-accounts/types"
+
+	errorsmod "cosmossdk.io/errors"
 )
 
 // CreateUpgradeHandler creates an SDK upgrade handler for v1.6.0
@@ -20,6 +33,41 @@ func CreateUpgradeHandler(
 		if err := restoreAccounts(ctx, ak); err != nil {
 			panic(err)
 		}
+
+		// create ICS27 Controller submodule params, with the controller module NOT enabled
+		gs := &genesistypes.GenesisState{
+			ControllerGenesisState: genesistypes.ControllerGenesisState{},
+			HostGenesisState: genesistypes.HostGenesisState{
+				Port: icatypes.HostPortID,
+				Params: icahosttypes.Params{
+					HostEnabled: true,
+					AllowMessages: []string{
+						sdk.MsgTypeURL(&banktypes.MsgSend{}),
+						sdk.MsgTypeURL(&banktypes.MsgMultiSend{}),
+						sdk.MsgTypeURL(&distrtypes.MsgSetWithdrawAddress{}),
+						sdk.MsgTypeURL(&distrtypes.MsgWithdrawDelegatorReward{}),
+						sdk.MsgTypeURL(&govtypes.MsgVote{}),
+						sdk.MsgTypeURL(&govtypes.MsgVoteWeighted{}),
+						sdk.MsgTypeURL(&stakingtypes.MsgDelegate{}),
+						sdk.MsgTypeURL(&stakingtypes.MsgUndelegate{}),
+						sdk.MsgTypeURL(&stakingtypes.MsgCancelUnbondingDelegation{}),
+						sdk.MsgTypeURL(&stakingtypes.MsgBeginRedelegate{}),
+						sdk.MsgTypeURL(&transfertypes.MsgTransfer{}),
+					},
+				},
+			},
+		}
+
+		bz, err := icatypes.ModuleCdc.MarshalJSON(gs)
+		if err != nil {
+			return nil, errorsmod.Wrapf(err, "failed to marshal %s genesis state", icatypes.ModuleName)
+		}
+
+		// Register the consensus version in the version map to avoid the SDK from triggering the default
+		// InitGenesis function.
+		vm[icatypes.ModuleName] = ica.AppModule{}.ConsensusVersion()
+
+		_ = mm.Modules[icatypes.ModuleName].InitGenesis(ctx, icatypes.ModuleCdc, bz)
 
 		return mm.RunMigrations(ctx, configurator, vm)
 	}
