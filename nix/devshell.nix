87a8f0bdaa006a171d6de3100f7930fba9f2dc4b
Andrey Kuznetsov <fear@loathing.in>
2023-11-20 17:48:08 +0400

    ci: check that proto files are in sync with generated files (#258)
    * ci: check that proto files are in sync with generated files
    
    * ci: update CODEOWNERS
    
    * ci: kioqq CODEOWNERS

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -1,10 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, pkgsUnstable, ... }:
 {
   dotenv.enable = true;
 
   packages = with pkgs;
     [
+      pkgsUnstable.act
       gh
+
       yarn
       nodejs
 
@@ -33,8 +35,15 @@
   scripts.ci-proto.exec = ''
     set -e
 
+    make clean
+    make proto-all
     make proto-swagger-gen
 
+    # it gets updated every time, so we are ignoring this one
+    git checkout -- client/docs/statik/statik.go
+
+    echo "Checking diff..."
+    
     if ! git diff --exit-code; then
     echo "Directory is not clean after swagger generation"
     exit 1
