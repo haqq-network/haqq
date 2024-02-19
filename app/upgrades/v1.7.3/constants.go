a4acbbe8b771e6d0ad36040197558d7ff30179b2
Petr Ivanov <krest2005@yandex.ru>
2024-02-19 15:19:01 +0400

    Implement legacy msg interface for liquid vesting (#297)
    * implement legacy msg interface
    
    * change msg namespace
    
    * chore: bump app version, add upgrade handler
    
    ---------
    
    Co-authored-by: Petr Ivanov <petr.ivanov@tendermint.com>
    Co-authored-by: Yuri Surbashev <yurist@insport.net>

---
+++ app/upgrades/v1.7.3/constants.go
@@ -0,0 +1,6 @@
+package v173
+
+const (
+	// UpgradeName is the shared upgrade plan name for mainnet and testnet
+	UpgradeName = "v1.7.3"
+)
--- x/liquidvesting/types/codec.go
+++ x/liquidvesting/types/codec.go
@@ -1,15 +1,45 @@
 package types
 
 import (
+	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
 )
 
+var (
+	amino = codec.NewLegacyAmino()
+	// AminoCdc is a amino codec created to support amino JSON compatible msgs.
+	AminoCdc = codec.NewAminoCodec(amino)
+)
+
+const (
+	// Amino names
+	liquidate = "haqq/MsgLiquidate"
+	redeem    = "haqq/MsgRedeem"
+)
+
+// NOTE: This is required for the GetSignBytes function
+func init() {
+	RegisterLegacyAminoCodec(amino)
+	amino.Seal()
+}
+
+// RegisterInterfaces associates protoName with AccountI and VestingAccount
+// Interfaces and creates a registry of it's concrete implementations
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 	registry.RegisterImplementations((*sdk.Msg)(nil),
 		&MsgLiquidate{},
 		&MsgRedeem{},
 	)
+
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
+}
+
+// RegisterLegacyAminoCodec registers the necessary x/erc20 interfaces and
+// concrete types on the provided LegacyAmino codec. These types are used for
+// Amino JSON serialization and EIP-712 compatibility.
+func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
+	cdc.RegisterConcrete(&MsgLiquidate{}, liquidate, nil)
+	cdc.RegisterConcrete(&MsgRedeem{}, redeem, nil)
 }
