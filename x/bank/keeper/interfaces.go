f54aaaa6d630ddf787b70a76f5229725c24e1a8d
Yuri Surbashev <yurist@insport.net>
2024-03-22 05:04:12 +0800

    chore: update bank

---
+++ x/bank/keeper/interfaces.go
@@ -0,0 +1,29 @@
+package keeper
+
+import (
+	"context"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/haqq-network/haqq/x/erc20/types"
+	evmtypes "github.com/haqq-network/haqq/x/evm/types"
+	"math/big"
+)
+
+type ERC20Keeper interface {
+	IsERC20Enabled(ctx sdk.Context) bool
+	GetTokenPairID(ctx sdk.Context, token string) []byte
+	GetTokenPair(ctx sdk.Context, id []byte) (types.TokenPair, bool)
+	GetTokenPairs(ctx sdk.Context) []types.TokenPair
+	IterateTokenPairs(ctx sdk.Context, cb func(tokenPair types.TokenPair) (stop bool))
+	BalanceOf(ctx sdk.Context, abi abi.ABI, contract, account common.Address) *big.Int
+	ConvertCoin(goCtx context.Context, msg *types.MsgConvertCoin) (*types.MsgConvertCoinResponse, error)
+	CallEVM(ctx sdk.Context, abi abi.ABI, from, contract common.Address, commit bool, method string, args ...interface{}) (*evmtypes.MsgEthereumTxResponse, error)
+}
+
+type AccountKeeper interface {
+	GetAccount(ctx context.Context, addr sdk.AccAddress) sdk.AccountI
+	SetAccount(ctx context.Context, account sdk.AccountI)
+	HasAccount(ctx context.Context, addr sdk.AccAddress) bool
+	NewAccountWithAddress(ctx context.Context, addr sdk.AccAddress) sdk.AccountI
+}
--- x/bank/keeper/msg_server.go
+++ x/bank/keeper/msg_server.go
@@ -5,7 +5,7 @@ import (
 	"math/big"
 
 	errorsmod "cosmossdk.io/errors"
-	"github.com/armon/go-metrics"
+	sdkmath "cosmossdk.io/math"
 	"github.com/cosmos/cosmos-sdk/telemetry"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
@@ -13,6 +13,7 @@ import (
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/hashicorp/go-metrics"
 
 	"github.com/haqq-network/haqq/contracts"
 	erc20types "github.com/haqq-network/haqq/x/erc20/types"
@@ -32,23 +33,39 @@ func NewMsgServerImpl(keeper WrappedBaseKeeper) types.MsgServer {
 var _ types.MsgServer = msgServer{}
 
 func (k msgServer) Send(goCtx context.Context, msg *types.MsgSend) (*types.MsgSendResponse, error) {
-	ctx := sdk.UnwrapSDKContext(goCtx)
+	var (
+		from, to []byte
+		err      error
+	)
 
+	if base, ok := k.Keeper.(BaseKeeper); ok {
+		from, err = base.ak.AddressCodec().StringToBytes(msg.FromAddress)
+		if err != nil {
+			return nil, sdkerrors.ErrInvalidAddress.Wrapf("invalid from address: %s", err)
+		}
+		to, err = base.ak.AddressCodec().StringToBytes(msg.ToAddress)
+		if err != nil {
+			return nil, sdkerrors.ErrInvalidAddress.Wrapf("invalid to address: %s", err)
+		}
+	} else {
+		return nil, sdkerrors.ErrInvalidRequest.Wrapf("invalid keeper type: %T", k.Keeper)
+	}
+
+	if !msg.Amount.IsValid() {
+		return nil, errorsmod.Wrap(sdkerrors.ErrInvalidCoins, msg.Amount.String())
+	}
+
+	if !msg.Amount.IsAllPositive() {
+		return nil, errorsmod.Wrap(sdkerrors.ErrInvalidCoins, msg.Amount.String())
+	}
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
 	if err := k.IsSendEnabledCoins(ctx, msg.Amount...); err != nil {
 		return nil, err
 	}
 
-	from, err := sdk.AccAddressFromBech32(msg.FromAddress)
-	if err != nil {
-		return nil, err
-	}
-	to, err := sdk.AccAddressFromBech32(msg.ToAddress)
-	if err != nil {
-		return nil, err
-	}
-
 	if k.BlockedAddr(to) {
-		return nil, sdkerrors.Wrapf(sdkerrors.ErrUnauthorized, "%s is not allowed to receive funds", msg.ToAddress)
+		return nil, errorsmod.Wrapf(sdkerrors.ErrUnauthorized, "%s is not allowed to receive funds", msg.ToAddress)
 	}
 
 	if err := k.sendCoinsWithERC20(ctx, from, to, msg.Amount); err != nil {
@@ -72,6 +89,22 @@ func (k msgServer) Send(goCtx context.Context, msg *types.MsgSend) (*types.MsgSe
 
 // MultiSend copy of original method. TODO Add ERC20 support
 func (k msgServer) MultiSend(goCtx context.Context, msg *types.MsgMultiSend) (*types.MsgMultiSendResponse, error) {
+	if len(msg.Inputs) == 0 {
+		return nil, types.ErrNoInputs
+	}
+
+	if len(msg.Inputs) != 1 {
+		return nil, types.ErrMultipleSenders
+	}
+
+	if len(msg.Outputs) == 0 {
+		return nil, types.ErrNoOutputs
+	}
+
+	if err := types.ValidateInputOutputs(msg.Inputs[0], msg.Outputs); err != nil {
+		return nil, err
+	}
+
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	// NOTE: totalIn == totalOut should already have been checked
@@ -82,14 +115,21 @@ func (k msgServer) MultiSend(goCtx context.Context, msg *types.MsgMultiSend) (*t
 	}
 
 	for _, out := range msg.Outputs {
-		accAddr := sdk.MustAccAddressFromBech32(out.Address)
+		if base, ok := k.Keeper.(BaseKeeper); ok {
+			accAddr, err := base.ak.AddressCodec().StringToBytes(out.Address)
+			if err != nil {
+				return nil, err
+			}
 
-		if k.BlockedAddr(accAddr) {
-			return nil, sdkerrors.Wrapf(sdkerrors.ErrUnauthorized, "%s is not allowed to receive transactions", out.Address)
+			if k.BlockedAddr(accAddr) {
+				return nil, errorsmod.Wrapf(sdkerrors.ErrUnauthorized, "%s is not allowed to receive funds", out.Address)
+			}
+		} else {
+			return nil, sdkerrors.ErrInvalidRequest.Wrapf("invalid keeper type: %T", k.Keeper)
 		}
 	}
 
-	err := k.InputOutputCoins(ctx, msg.Inputs, msg.Outputs)
+	err := k.InputOutputCoins(ctx, msg.Inputs[0], msg.Outputs)
 	if err != nil {
 		return nil, err
 	}
@@ -99,7 +139,11 @@ func (k msgServer) MultiSend(goCtx context.Context, msg *types.MsgMultiSend) (*t
 
 func (k msgServer) UpdateParams(goCtx context.Context, req *types.MsgUpdateParams) (*types.MsgUpdateParamsResponse, error) {
 	if k.GetAuthority() != req.Authority {
-		return nil, sdkerrors.Wrapf(govtypes.ErrInvalidSigner, "invalid authority; expected %s, got %s", k.GetAuthority(), req.Authority)
+		return nil, errorsmod.Wrapf(govtypes.ErrInvalidSigner, "invalid authority; expected %s, got %s", k.GetAuthority(), req.Authority)
+	}
+
+	if err := req.Params.Validate(); err != nil {
+		return nil, err
 	}
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
@@ -112,7 +156,26 @@ func (k msgServer) UpdateParams(goCtx context.Context, req *types.MsgUpdateParam
 
 func (k msgServer) SetSendEnabled(goCtx context.Context, msg *types.MsgSetSendEnabled) (*types.MsgSetSendEnabledResponse, error) {
 	if k.GetAuthority() != msg.Authority {
-		return nil, sdkerrors.Wrapf(govtypes.ErrInvalidSigner, "invalid authority; expected %s, got %s", k.GetAuthority(), msg.Authority)
+		return nil, errorsmod.Wrapf(govtypes.ErrInvalidSigner, "invalid authority; expected %s, got %s", k.GetAuthority(), msg.Authority)
+	}
+
+	seen := map[string]bool{}
+	for _, se := range msg.SendEnabled {
+		if _, alreadySeen := seen[se.Denom]; alreadySeen {
+			return nil, sdkerrors.ErrInvalidRequest.Wrapf("duplicate denom entries found for %q", se.Denom)
+		}
+
+		seen[se.Denom] = true
+
+		if err := se.Validate(); err != nil {
+			return nil, sdkerrors.ErrInvalidRequest.Wrapf("invalid SendEnabled denom %q: %s", se.Denom, err)
+		}
+	}
+
+	for _, denom := range msg.UseDefaultFor {
+		if err := sdk.ValidateDenom(denom); err != nil {
+			return nil, sdkerrors.ErrInvalidRequest.Wrapf("invalid UseDefaultFor denom %q: %s", denom, err)
+		}
 	}
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
@@ -172,7 +235,7 @@ func (k msgServer) sendCoinsWithERC20(ctx sdk.Context, from sdk.AccAddress, to s
 
 func (k msgServer) subUnlockedERC20Tokens(ctx sdk.Context, tokenPair erc20types.TokenPair, fromAddr sdk.AccAddress, toAddr sdk.AccAddress, amt sdk.Coin) error {
 	if !amt.IsValid() {
-		return sdkerrors.Wrap(sdkerrors.ErrInvalidCoins, amt.String())
+		return errorsmod.Wrap(sdkerrors.ErrInvalidCoins, amt.String())
 	}
 
 	lockedCoins := k.LockedCoins(ctx, fromAddr)
@@ -189,11 +252,11 @@ func (k msgServer) subUnlockedERC20Tokens(ctx sdk.Context, tokenPair erc20types.
 	if evmFromBalanceToken == nil {
 		return errorsmod.Wrap(erc20types.ErrEVMCall, "failed to retrieve sender's balance")
 	}
-	evmBalance := sdk.NewCoin(tokenPair.Denom, sdk.NewIntFromBigInt(evmFromBalanceToken))
+	evmBalance := sdk.NewCoin(tokenPair.Denom, sdkmath.NewIntFromBigInt(evmFromBalanceToken))
 
 	_, hasNeg := sdk.Coins{spendable.Add(evmBalance)}.SafeSub(amt)
 	if hasNeg {
-		return sdkerrors.Wrapf(sdkerrors.ErrInsufficientFunds, "%s is smaller than %s", spendable, amt)
+		return errorsmod.Wrapf(sdkerrors.ErrInsufficientFunds, "%s is smaller than %s", spendable, amt)
 	}
 
 	if !spendable.IsZero() {
