d0fa9ea9414b70bc6f188a6fcba66151f91ec6be
Yuri Surbashev <yurist@insport.net>
2024-04-24 22:57:40 +0800

    chore: update feemarket tests

--- x/feemarket/keeper/setup_test.go
+++ x/feemarket/keeper/setup_test.go
@@ -1,57 +1,47 @@
 package keeper_test
 
 import (
-	"testing"
-
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-
 	"github.com/stretchr/testify/suite"
 
-	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/codec"
-	"github.com/cosmos/cosmos-sdk/crypto/keyring"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/ethereum/go-ethereum/common"
-	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/cosmos/cosmos-sdk/baseapp"
 
-	"github.com/haqq-network/haqq/app"
-	"github.com/haqq-network/haqq/utils"
-	"github.com/haqq-network/haqq/x/feemarket/types"
+	"github.com/haqq-network/haqq/testutil/integration/haqq/factory"
+	"github.com/haqq-network/haqq/testutil/integration/haqq/grpc"
+	testkeyring "github.com/haqq-network/haqq/testutil/integration/haqq/keyring"
+	"github.com/haqq-network/haqq/testutil/integration/haqq/network"
 )
 
 type KeeperTestSuite struct {
 	suite.Suite
 
-	ctx         sdk.Context
-	app         *app.Haqq
-	queryClient types.QueryClient
-	address     common.Address
-	consAddress sdk.ConsAddress
+	network     *network.UnitTestNetwork
+	factory     factory.TxFactory
+	grpcHandler grpc.Handler
+	keyring     testkeyring.Keyring
 
-	// for generate test tx
-	clientCtx client.Context
-	ethSigner ethtypes.Signer
-
-	appCodec codec.Codec
-	signer   keyring.Signer
-	denom    string
+	denom string
 }
 
-var s *KeeperTestSuite
+// SetupTest setup test environment
+func (s *KeeperTestSuite) SetupTest() {
+	keyring := testkeyring.New(2)
+	nw := network.NewUnitTestNetwork(
+		network.WithPreFundedAccounts(keyring.GetAllAccAddrs()...),
+		network.WithCustomBaseAppOpts(baseapp.SetMinGasPrices("10aevmos")),
+	)
+	grpcHandler := grpc.NewIntegrationHandler(nw)
+	txFactory := factory.New(nw, grpcHandler)
 
-func TestKeeperTestSuite(t *testing.T) {
-	s = new(KeeperTestSuite)
-	suite.Run(t, s)
+	ctx := nw.GetContext()
+	sk := nw.App.StakingKeeper
+	bondDenom, err := sk.BondDenom(ctx)
+	if err != nil {
+		panic(err)
+	}
 
-	// Run Ginkgo integration tests
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "Keeper Suite")
-}
-
-// SetupTest setup test environment, it uses`require.TestingT` to support both `testing.T` and `testing.B`.
-func (suite *KeeperTestSuite) SetupTest() {
-	chainID := utils.TestEdge2ChainID + "-1"
-	suite.app, _ = app.Setup(false, nil, chainID)
-	suite.SetupApp(false, chainID)
+	s.denom = bondDenom
+	s.factory = txFactory
+	s.grpcHandler = grpcHandler
+	s.keyring = keyring
+	s.network = nw
 }
