9f0699d383d9afd65868e0d941020d36c5b92825
Petr Ivanov <petr.ivanov@tendermint.com>
2024-06-24 21:47:52 +0400

    add params validate tests

---
+++ x/shariahoracle/types/params_test.go
@@ -0,0 +1,52 @@
+package types_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+
+	"github.com/haqq-network/haqq/x/shariahoracle/types"
+)
+
+type ParamsTestSuite struct {
+	suite.Suite
+}
+
+func TestParamsTestSuite(t *testing.T) {
+	suite.Run(t, new(ParamsTestSuite))
+}
+
+func (suite *ParamsTestSuite) TestParamsValidate() {
+	testCases := []struct {
+		name     string
+		params   types.Params
+		expError bool
+	}{
+		{"default", types.DefaultParams(), false},
+		{
+			"valid",
+			types.NewParams("0xdac17f958d2ee523a2206206994597c13d831ec7"),
+			false,
+		},
+		{
+			"empty address",
+			types.NewParams(""),
+			true,
+		},
+		{
+			"empty",
+			types.Params{},
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		err := tc.params.Validate()
+
+		if tc.expError {
+			suite.Require().Error(err, tc.name)
+		} else {
+			suite.Require().NoError(err, tc.name)
+		}
+	}
+}
