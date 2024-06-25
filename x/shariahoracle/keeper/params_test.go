1715345cc6905ea750645f976884a56a789d066a
Petr Ivanov <petr.ivanov@tendermint.com>
2024-06-25 10:20:17 +0400

    add params keeper tests

---
+++ x/shariahoracle/keeper/params_test.go
@@ -0,0 +1,37 @@
+package keeper_test
+
+import (
+	"reflect"
+
+	"github.com/haqq-network/haqq/x/shariahoracle/types"
+)
+
+func (suite *KeeperTestSuite) TestParams() {
+	params := suite.app.ShariaOracleKeeper.GetParams(suite.ctx)
+	suite.app.ShariaOracleKeeper.SetParams(suite.ctx, params) //nolint:errcheck
+
+	testCases := []struct {
+		name      string
+		paramsFun func() interface{}
+		getFun    func() interface{}
+		expected  bool
+	}{
+		{
+			"success - Checks if the default params are set correctly",
+			func() interface{} {
+				return types.DefaultParams()
+			},
+			func() interface{} {
+				return suite.app.ShariaOracleKeeper.GetParams(suite.ctx)
+			},
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.Run(tc.name, func() {
+			outcome := reflect.DeepEqual(tc.paramsFun(), tc.getFun())
+			suite.Require().Equal(tc.expected, outcome)
+		})
+	}
+}
