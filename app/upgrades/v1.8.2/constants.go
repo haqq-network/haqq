ea607776dc27cc2d511231733beac167dc8f4eb3
Yuri Surbashev <yurist@insport.net>
2025-10-01 14:57:58 +0800

    chore: restore dummy upgrades for e2e tests

---
+++ app/upgrades/v1.8.2/constants.go
@@ -0,0 +1,5 @@
+package v182
+
+// UpgradeName is the shared upgrade plan name for mainnet
+// Note: this file is here just for the e2e tests
+const UpgradeName = "v1.8.2"
---
+++ app/upgrades/v1.8.4/constants.go
@@ -0,0 +1,5 @@
+package v184
+
+// UpgradeName is the shared upgrade plan name for mainnet
+// Note: this file is here just for the e2e tests
+const UpgradeName = "v1.8.4"
