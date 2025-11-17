76c71e76899f82781eeec63fbb5c10887c70ac35
kenta-mori3322 <kenta.mori32@gmail.com>
2025-11-17 13:44:58 +0000

    chore: add erc20 middleware for ibc v2

--- x/ibc/transfer/keeper/keeper.go
+++ x/ibc/transfer/keeper/keeper.go
@@ -1,13 +1,13 @@
 package keeper
 
 import (
-    corestore "cosmossdk.io/core/store"
-    "github.com/cosmos/cosmos-sdk/codec"
-    "github.com/cosmos/ibc-go/v10/modules/apps/transfer/keeper"
-    transfertypes "github.com/cosmos/ibc-go/v10/modules/apps/transfer/types"
-    porttypes "github.com/cosmos/ibc-go/v10/modules/core/05-port/types"
+	corestore "cosmossdk.io/core/store"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/ibc-go/v10/modules/apps/transfer/keeper"
+	transfertypes "github.com/cosmos/ibc-go/v10/modules/apps/transfer/types"
+	porttypes "github.com/cosmos/ibc-go/v10/modules/core/05-port/types"
 
-    "github.com/haqq-network/haqq/x/ibc/transfer/types"
+	"github.com/haqq-network/haqq/x/ibc/transfer/types"
 )
 
 // Keeper defines the modified IBC transfer keeper that embeds the original one.
@@ -22,25 +22,25 @@ type Keeper struct {
 
 // NewKeeper creates a new IBC transfer Keeper instance
 func NewKeeper(
-    cdc codec.BinaryCodec,
-    storeService corestore.KVStoreService,
-    paramSubspace transfertypes.ParamSubspace,
+	cdc codec.BinaryCodec,
+	storeService corestore.KVStoreService,
+	paramSubspace transfertypes.ParamSubspace,
 
-    ics4Wrapper porttypes.ICS4Wrapper,
-    channelKeeper transfertypes.ChannelKeeper,
-    msgRouter transfertypes.MessageRouter,
-    accountKeeper types.AccountKeeper,
-    bankKeeper types.BankKeeper,
-    erc20Keeper types.ERC20Keeper,
-    authority string,
+	ics4Wrapper porttypes.ICS4Wrapper,
+	channelKeeper transfertypes.ChannelKeeper,
+	msgRouter transfertypes.MessageRouter,
+	accountKeeper types.AccountKeeper,
+	bankKeeper types.BankKeeper,
+	erc20Keeper types.ERC20Keeper,
+	authority string,
 ) Keeper {
 	// create the original IBC transfer keeper for embedding
-    transferKeeper := keeper.NewKeeper(
-        cdc, storeService, paramSubspace,
-        ics4Wrapper, channelKeeper, msgRouter,
-        accountKeeper, bankKeeper,
-        authority,
-    )
+	transferKeeper := keeper.NewKeeper(
+		cdc, storeService, paramSubspace,
+		ics4Wrapper, channelKeeper, msgRouter,
+		accountKeeper, bankKeeper,
+		authority,
+	)
 
 	return Keeper{
 		Keeper:        &transferKeeper,
