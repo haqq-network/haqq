bf3871c2418c57679e6daf407b538d517e1601b7
kenta-mori3322 <kenta.mori32@gmail.com>
2025-11-20 09:54:43 +0000

    fix: lint check

--- nix/devshell/common.nix
+++ nix/devshell/common.nix
@@ -6,6 +6,7 @@
       buf
       clang-tools
       codespell
+      jq
 
       go
       (pkgsUnstable.gomod2nix.override {
@@ -16,4 +17,14 @@
         inherit pkgs;
       })
     ];
+  
+  # Install statik via go install since it's not available as a nix package
+  enterShell = ''
+    export GOPATH="''${GOPATH:-$HOME/go}"
+    export PATH="$GOPATH/bin:$PATH"
+    if ! command -v statik &> /dev/null; then
+      echo "Installing statik..."
+      (cd /tmp && go install github.com/rakyll/statik@v0.1.6)
+    fi
+  '';
 }
