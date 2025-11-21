d1dbcca6504adf0535503bf59309ecfe146ed125
kenta-mori3322 <kenta.mori32@gmail.com>
2025-11-21 09:31:38 +0000

    chore: add missing CLIs in ucdao

--- x/ucdao/client/cli/query.go
+++ x/ucdao/client/cli/query.go
@@ -34,7 +34,51 @@ func GetQueryCmd() *cobra.Command {
 		GetBalancesCmd(),
 		GetCmdQueryTotalBalance(),
 		GetCmdQueryEscrowAddress(),
+		GetCmdQueryHolders(),
+		GetCmdQueryParams(),
 	)
+
+	return cmd
+}
+
+func GetBalanceCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "balance [address] [denom]",
+		Short: "Query the balance of a given address for a given denomination",
+		Args:  cobra.ExactArgs(2),
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Query the balance of a given address for a given denomination.
+
+Example:
+  $ %s query %s balance [address] [denom]
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+			ctx := cmd.Context()
+
+			addr, err := sdk.AccAddressFromBech32(args[0])
+			if err != nil {
+				return err
+			}
+
+			denom := args[1]
+
+			res, err := queryClient.Balance(ctx, &types.QueryBalanceRequest{Address: addr.String(), Denom: denom})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res.Balance)
+		},
+	}
 
 	return cmd
 }
@@ -180,6 +224,83 @@ Example:
 			}
 
 			res, err := queryClient.EscrowAddress(ctx, &types.QueryEscrowAddressRequest{Address: addr.String()})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	return cmd
+}
+
+func GetCmdQueryHolders() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "holders",
+		Short: "Query the holders of the United Contributors DAO",
+		Args:  cobra.NoArgs,
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Query the holders of the United Contributors DAO.
+
+Example:
+  $ %s query %s holders
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+			ctx := cmd.Context()
+
+			pageReq, err := client.ReadPageRequest(cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			res, err := queryClient.Holders(ctx, &types.QueryHoldersRequest{Pagination: pageReq})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	flags.AddPaginationFlagsToCmd(cmd, "all holders")
+
+	return cmd
+}
+
+func GetCmdQueryParams() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "params",
+		Short: "Query the parameters of the United Contributors DAO",
+		Args:  cobra.NoArgs,
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Query the parameters of the United Contributors DAO.
+
+Example:
+  $ %s query %s params
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+			ctx := cmd.Context()
+			res, err := queryClient.Params(ctx, &types.QueryParamsRequest{})
 			if err != nil {
 				return err
 			}
